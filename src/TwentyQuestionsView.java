13a066d08e77bb44c6642b978e3e9cdf42361d0f
Jessica Bahny <77072076+silversun307@users.noreply.github.com>
2022-01-02 18:02:44 -0800

    Add files via upload

---
+++ src/TwentyQuestionsView.java
@@ -0,0 +1,38 @@
+public class TwentyQuestionsView {
+
+    public void splash() {
+        //TODO Student
+    }
+
+    public void welcome(){
+        System.out.println("Welcome to Twenty Questions.\nPlease enter player name: ");
+    }
+
+
+    public void tooHigh(){
+        System.out.println("Too high.");
+    }
+
+    public void tooLow(){
+        System.out.println("Too low.");
+    }
+
+
+    public void winnerMessage(){
+        //TODO student
+        System.out.println("Your guess is correct! Congratulations!\n");
+    }
+
+
+    public void loserMessage(){
+        //TODO student
+        System.out.println("You ran out of guesses. Better luck next time!\n");
+    }
+
+
+    public void exitGame() {
+        System.out.println("Thank you for playing!\n");
+    }
+
+
+}
